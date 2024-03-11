25c664d3a4d813e3c4ff0367bd1b5fe5a1f421c2
dexterz-m <migunok12@gmail.com>
2024-03-11 23:34:50 +0200

    Finished styling and started js

---
+++ main.js
@@ -0,0 +1,39 @@
+// Assigned values or presets
+
+let firstValue
+let secondValue
+let operator
+
+// Functions
+
+function addValue(){
+
+}
+
+function allClear(){
+
+}
+
+function reverse(){
+    
+}
+
+function percent(){
+
+}
+
+function division(){
+
+}
+
+function multiplication(){
+
+}
+
+function substraction(){
+    
+}
+
+function addition(){
+
+}
