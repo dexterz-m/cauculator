b46dfd35b582c7f9fa08600b3fa7895388ebdb73
dexterz-m <migunok12@gmail.com>
2024-03-12 02:03:59 +0200

    Made usable cauculator

--- main.js
+++ main.js
@@ -4,36 +4,72 @@ let firstValue
 let secondValue
 let operator
 
+let displayValue = ''
+
+document.getElementById('input').innerHTML = displayValue
+
 // Functions
 
-function addValue(){
+function addValue(inputValue){
+    if(inputValue === '0'){
+        displayValue += '0.'
+        document.getElementById('input').innerHTML = displayValue
+    }
 
+    else if(inputValue === '.'){
+        if(displayValue.indexOf('.') == -1){
+            displayValue += inputValue
+        }
+    }
+
+    else{
+    displayValue += inputValue
+    document.getElementById('input').innerHTML = displayValue
+}
+}
+
+function addOperator(inputOperator){
+    operator = inputOperator
+    firstValue = parseFloat(displayValue)
+    displayValue = ''
+
+    if(operator === '+/-'){
+        displayValue = firstValue * -1
+        document.getElementById('input').innerHTML = displayValue
+    }
+
+    else if(operator === '%'){
+        displayValue = firstValue / 100
+        document.getElementById('input').innerHTML = displayValue
+    }
 }
 
 function allClear(){
-
+    displayValue = ''
+    firstValue = ''
+    secondValue = ''
+    operator = ''
+    document.getElementById('input').innerHTML = displayValue
 }
 
-function reverse(){
+function evaluation(){
+    secondValue = parseFloat(displayValue)
     
-}
+    if(operator === '+'){
+        displayValue = firstValue + secondValue
+    }
 
-function percent(){
+    else if(operator === '-'){
+        displayValue = firstValue - secondValue
+    }
 
-}
+    else if(operator === '*'){
+        displayValue = firstValue * secondValue
+    }
 
-function division(){
+    else if(operator === '/'){
+        displayValue = firstValue / secondValue
+    }
 
-}
-
-function multiplication(){
-
-}
-
-function substraction(){
-    
-}
-
-function addition(){
-
+    document.getElementById('input').innerHTML = displayValue
 }
