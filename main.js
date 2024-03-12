45e210a7d9c669eb03e96461fba93bbb3815df19
dexterz-m <migunok12@gmail.com>
2024-03-12 02:27:31 +0200

    Added favicon and some code comments

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ document.getElementById('input').innerHTML = displayValue
 
 function addValue(inputValue){
 
+    // Not allowing to use more than one dot (decimal) for one number
+
     if(inputValue === '.'){
         if(displayValue.indexOf('.') == -1){
             displayValue += inputValue
@@ -31,16 +33,22 @@ function addOperator(inputOperator){
     firstValue = parseFloat(displayValue)
     displayValue = ''
 
+    // Reversing value
+
     if(operator === '+/-'){
         displayValue = firstValue * -1
         document.getElementById('input').innerHTML = displayValue
     }
+
+    // Getting percent
 
     else if(operator === '%'){
         displayValue = firstValue / 100
         document.getElementById('input').innerHTML = displayValue
     }
 }
+
+// Reseting all values
 
 function allClear(){
     displayValue = ''
@@ -50,20 +58,30 @@ function allClear(){
     document.getElementById('input').innerHTML = displayValue
 }
 
+// Getting evaluation based on inputed operator
+
 function evaluation(){
     secondValue = parseFloat(displayValue)
-    
+
+    // Addition
+
     if(operator === '+'){
         displayValue = firstValue + secondValue
     }
+
+    // Substraction
 
     else if(operator === '-'){
         displayValue = firstValue - secondValue
     }
 
+    // Multiplying
+
     else if(operator === '*'){
         displayValue = firstValue * secondValue
     }
+
+    // Dividing
 
     else if(operator === '/'){
         displayValue = firstValue / secondValue
